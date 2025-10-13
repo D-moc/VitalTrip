fb94d547e5a472c10ec21c06b07b293f40b81e16
Omkar Gupta <omkar.gupta@somaiya.edu>
2025-10-13 15:52:38 +0530

    Add backend

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const server = express();
+const path = requrire('path');
+
+
+const staticPath = path.join(___dirname,".../public")
+
+server.use(express.static(staticPath));
+
+server.get("/",(req,res)=>{
+    res.write("<h1>Hello Omkar & Dinesh</h1>")
+    res.send();
+});
+
+server.listen(4000)
