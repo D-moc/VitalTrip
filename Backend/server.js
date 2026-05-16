34105a91f83a6d09b2118eaa9c98e24a22ac645a
D-moc <karmadinesh159@gmail.com>
2026-05-16 20:30:55 +0530

    vercel fixed

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,28 @@
+// import http from "http";
+// import app from "./app.js";
+
+// const PORT = process.env.PORT || 4000;
+
+// const server = http.createServer(app);
+
+// server.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+
 import http from "http";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 4000;
 
-const server = http.createServer(app);
+/* Local Development */
+if (process.env.NODE_ENV !== "production") {
+  const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+  server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+/* Vercel Deployment */
+export default app;
