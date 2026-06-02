3ede211d33a79a1a5d119f5f9d1d44050ccd6021
D-moc <karmadinesh159@gmail.com>
2026-06-02 19:59:01 +0530

    lowercase middleware filename

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { body } from "express-validator";
-import upload from "../middlewares/temp.js";
+import upload from "../middlewares/upload.middleware.js";
 import { authUser } from "../middlewares/auth.middleware.js";
 
 import {
