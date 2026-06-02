fa31a939238c4365072211cfd49f9eb4abd9728e
D-moc <karmadinesh159@gmail.com>
2026-06-02 21:25:29 +0530

    roleAuth fixed finally

--- Backend/routes/captainDashboard.routes.js
+++ Backend/routes/captainDashboard.routes.js
@@ -14,7 +14,7 @@ import {
   getAllUsersWithTrips,
   getUpcomingTrips,
 } from "../controllers/captainDashboard.controller.js";
-import { verifyCaptainAccess } from "../middlewares/roleAuth.middleware.js";
+import { verifyCaptainAccess } from "../middlewares/roleAuth.middleware..js";
 
 const router = express.Router();
 
